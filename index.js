37fdc52c04ff941cb718a729dd2914020fd737f7
Emrandu1989 <emrandu1989@gmail.com>
2024-06-06 05:22:52 +0600

    details route added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -44,6 +44,13 @@ async function run() {
      app.get('/allEmployee', async(req, res)=>{
          const result = await userCollection.find().toArray();
          res.send(result);
+     })
+
+     app.get('/allEmployee/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await userCollection.findOne(query);
+        res.send(result)
      })
 
 
